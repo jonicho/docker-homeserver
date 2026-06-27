f95a206a5b26a9302e451ccc2b73f49bf28a2ae1
Jonas Richard Keller <jonas.r.keller@gmail.com>
2026-06-27 20:20:23 +0200

    Forward ssh port from gateway for Forgejo

--- gateway/ifscript.sh
+++ gateway/ifscript.sh
@@ -22,8 +22,10 @@ fi
 
 iptables -$iptables_append_arg FORWARD -i $phy_interface -o $wg_interface -p tcp --syn --dport 80 -m conntrack --ctstate NEW -j ACCEPT
 iptables -$iptables_append_arg FORWARD -i $phy_interface -o $wg_interface -p tcp --syn --dport 443 -m conntrack --ctstate NEW -j ACCEPT
+iptables -$iptables_append_arg FORWARD -i $phy_interface -o $wg_interface -p tcp --syn --dport 22 -m conntrack --ctstate NEW -j ACCEPT
 iptables -$iptables_append_arg FORWARD -i $phy_interface -o $wg_interface -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -$iptables_append_arg FORWARD -i $wg_interface -o $phy_interface -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -t nat -$iptables_append_arg PREROUTING -i $phy_interface -p tcp --dport 80 -j DNAT --to-destination 10.0.0.2
 iptables -t nat -$iptables_append_arg PREROUTING -i $phy_interface -p tcp --dport 443 -j DNAT --to-destination 10.0.0.2
+iptables -t nat -$iptables_append_arg PREROUTING -i $phy_interface -p tcp --dport 22 -j DNAT --to-destination 10.0.0.3
 iptables -t nat -$iptables_append_arg POSTROUTING -o $phy_interface -j MASQUERADE
